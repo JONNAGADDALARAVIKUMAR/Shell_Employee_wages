92c8e837fe65b98f95810686c69dfd054b0387d4
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-05 07:24:22 +0530

    Employee wage per a day

--- Employee.sh
+++ Employee.sh
@@ -1,2 +1,12 @@
 #!/bin/bash/ -x
-echo -e "\n\tWelcome to Employee Wage Computation programme"
+wageHour=20
+dayHours=8
+echo -e "\n\n\t\t\tWelcome to Employee Wage Computation Program"
+randValue=$(($RANDOM%10))
+if [[ $randValue -le 6 ]]
+then
+echo -e "\n\tEmployee is present"
+echo -e "\n\tWage : "$(($wageHour*$dayHours))
+else
+echo -e "\n\tEmployee is absent"
+fi
