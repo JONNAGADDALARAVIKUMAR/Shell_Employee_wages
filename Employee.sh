2644c716660f6289826e6342c13e1f13b8607cf5
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-05 08:03:27 +0530

    Switch case statement

--- Employee.sh
+++ Employee.sh
@@ -4,17 +4,24 @@ dayHours=8
 PartHours=4
 echo -e "\n\n\t\t\tWelcome to Employee Wage Computation Program"
 randValue=$(($RANDOM%10))
+echo -n -e "\n\n\tSelect type of Employee\n\t\t1.Full time Emloyee\n\t\t2.Full time Employee\n\tOPTION : "
+read empType
+case $empType in
+1)
 if [[ $randValue -le 6 ]]
 then
 echo -e "\n\tFull time Employee is present"
 echo -e "\n\tWage : "$(($wageHour*$dayHours))
 else
 echo -e "\n\tFull time Employee is absent"
-fi
+fi ;;
+
+2)
 if [[ $randValue -le 6 ]]
 then
 echo -e "\n\tPart time Employee is present"
 echo -e "\n\tWage : "$(($wageHour*$PartHours))
 else
 echo -e "\n\tPart time Employee is absent"
-fi
+fi ;;
+esac
