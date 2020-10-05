c42aae5eb41af407fa18e4b2f90d79b499e3c5eb
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-05 07:44:57 +0530

    Part time Employee Added

--- Employee.sh
+++ Employee.sh
@@ -1,12 +1,20 @@
 #!/bin/bash/ -x
 wageHour=20
 dayHours=8
+PartHours=4
 echo -e "\n\n\t\t\tWelcome to Employee Wage Computation Program"
 randValue=$(($RANDOM%10))
 if [[ $randValue -le 6 ]]
 then
-echo -e "\n\tEmployee is present"
+echo -e "\n\tFull time Employee is present"
 echo -e "\n\tWage : "$(($wageHour*$dayHours))
 else
-echo -e "\n\tEmployee is absent"
+echo -e "\n\tFull time Employee is absent"
+fi
+if [[ $randValue -le 6 ]]
+then
+echo -e "\n\tPart time Employee is present"
+echo -e "\n\tWage : "$(($wageHour*$PartHours))
+else
+echo -e "\n\tPart time Employee is absent"
 fi
