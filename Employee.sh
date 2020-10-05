1614c0472915864f8178b5f519d727aa8428708f
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-05 07:14:37 +0530

    Employee Present or not

--- Employee.sh
+++ Employee.sh
@@ -1,2 +1,9 @@
 #!/bin/bash/ -x
 echo -e "\n\tWelcome to Employee Wage Computation programme"
+randValue=$(($RANDOM%10))
+if [ $randValue -le 6 ]
+then
+echo -e "\n\tEmployee is present"
+else
+echo -e "\n\tEmployee is absent"
+fi
